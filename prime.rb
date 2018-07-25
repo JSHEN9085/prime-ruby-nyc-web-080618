3ae6e8336a9b06ad28d158b9507e15ffcc2d21d1
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 00:56:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,25 @@
 # Add  code here!
-def prime?(num)
-  if num < 2 
-    return false 
+# def prime?(num)
+#   if num < 2 
+#     return false 
     
-  else 
-    i = 2 
-    while i < num do 
-      if num % i == 0 
-        return false 
+#   else 
+#     i = 2 
+#     while i < num do 
+#       if num % i == 0 
+#         return false 
         
-        i += 1 
-      end 
-      return true 
-    end 
+#         i += 1 
+#       end 
+#       return true 
+#     end 
+    
+    def is_prime n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
+
+  true
+ end
