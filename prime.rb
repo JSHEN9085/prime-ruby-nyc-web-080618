ce2123855a657e0c00d7c4bf870a741a90fdc739
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 00:59:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 # Add  code here!
-# def prime?(num)
-#   if num < 2 
-#     return false 
+def prime?(num)
+  if num < 2 
+    return false 
     
-#   else 
-#     i = 2 
-#     while i < num do 
-#       if num % i == 0 
-#         return false 
+  else 
+    i = 2 
+    while i < num do 
+      if num % i == 0 
+        return false 
         
-#         i += 1 
-#       end 
-#       return true 
-#     end 
+        i += 1 
+      end 
+      return true 
+    end 
     
