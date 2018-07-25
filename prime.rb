7a4388667f5dd386ed56a3d8d06a8283d1c8659a
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 00:58:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -14,19 +14,3 @@
 #       return true 
 #     end 
     
-def isPrime(number)
-	if number == 0 or number == 1
-		return false
-	end
-
-	i = 2
-	limit = number / i
-	while i < limit
-		if number % i == 0
-			return false
-		end
-		i += 1
-		limit = number / i
-	end
-	return true
-end
